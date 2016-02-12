4685efe8e11d827b63e4df7eca122e4ce523bb9f
vic kim <yourboyvic@gmail.com>
2016-02-11 21:49:25 -0800

    checkpoint 35 assignment completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
    it { is_expected.to have_many(:posts) }
-
+   it { is_expected.to validate_length_of(:name).is_at_least(5) }
+   it { is_expected.to validate_length_of(:description).is_at_least(15) }
 # #1
 describe "attributes" do
   it "responds to name" do
