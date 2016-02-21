1bcb25889bcc92cc47254be06f70b056c6c633ce
vic kim <yourboyvic@gmail.com>
2016-02-21 08:42:33 -0800

    checkpoint 44 done

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
       resources :posts, only: [] do
 
         resources :comments, only: [:create, :destroy]
+        resources :favorites, only: [:create, :destroy]
 
         post '/up-vote' => 'votes#up_vote', as: :up_vote
-        post '/down-vote' => 'votes#down_vote', as: :down_vote        
+        post '/down-vote' => 'votes#down_vote', as: :down_vote
       end
 
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
+require 'random_data'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
+  it { is_expected.to have_many(:favorites) }
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
@@ -29,24 +31,24 @@ RSpec.describe User, type: :model do
       expect(user).to respond_to(:role)
     end
 
-# #2
+
     it "responds to admin?" do
       expect(user).to respond_to(:admin?)
     end
 
-# #3
+
     it "responds to member?" do
       expect(user).to respond_to(:member?)
     end
   end
 
   describe "roles" do
-# #4
+
     it "is member by default" do
       expect(user.role).to eql("member")
     end
 
-# #5
+
     context "member user" do
       it "returns true for #member?" do
         expect(user.member?).to be_truthy
@@ -57,7 +59,7 @@ RSpec.describe User, type: :model do
       end
     end
 
-# #6
+
     context "admin user" do
       before do
         user.admin!
@@ -95,4 +97,22 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email_format).to_not be_valid
     end
 
+    describe "#favorite_for(post)" do
+      before do
+        topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+        @post = topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)
+      end
+
+      it "returns `nil` if the user has not favorited the post" do
+
+        expect(user.favorite_for(@post)).to be_nil
+      end
+
+      it "returns the appropriate favorite if it exists" do
+
+        favorite = user.favorites.where(post: @post).create
+
+        expect(user.favorite_for(@post)).to eq(favorite)
+      end
+    end
   end
