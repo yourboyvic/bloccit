8d5ee195fadb3f4fd7e474df8fb19251f9458516
vic kim <yourboyvic@gmail.com>
2016-02-21 00:35:13 -0800

    checkpoint 43 completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
       resources :posts, except: [:index]
     end
 
-    # #4
+
       resources :posts, only: [] do
-    # #5
+
         resources :comments, only: [:create, :destroy]
+
+        post '/up-vote' => 'votes#up_vote', as: :up_vote
+        post '/down-vote' => 'votes#down_vote', as: :down_vote        
       end
 
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'random_data'
 
 RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
@@ -11,24 +12,77 @@ RSpec.describe Post, type: :model do
   it { is_expected.to have_many(:labels).through(:labelings) }
 
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_presence_of(:topic) }
-  it { is_expected.to validate_presence_of(:user) }
+  #it { is_expected.to validate_presence_of(:user) }
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
 
   describe "attributes" do
-# #2
+
     it "responds to title" do
       expect(post).to respond_to(:title)
     end
-# #3
+
     it "responds to body" do
       expect(post).to respond_to(:body)
+    end
+  end
+
+
+  describe "voting" do
+
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+      @up_votes = post.votes.where(value: 1).count
+      @down_votes = post.votes.where(value: -1).count
+    end
+
+
+    describe "#up_votes" do
+      it "counts the number of votes with value = 1" do
+        expect( post.up_votes ).to eq(@up_votes)
+      end
+    end
+
+
+    describe "#down_votes" do
+      it "counts the number of votes with value = -1" do
+        expect( post.down_votes ).to eq(@down_votes)
+      end
+    end
+
+
+    describe "#points" do
+      it "returns the sum of all down and up votes" do
+        expect( post.points ).to eq(1)
+      end
+    end
+
+    describe "#update_rank" do
+# #28
+      it "calculates the correct rank" do
+        post.update_rank
+        expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
+      end
+
+      it "updates the rank when an up vote is created" do
+        old_rank = post.rank
+        post.votes.create!(value: 1)
+        expect(post.rank).to eq (old_rank + 1)
+      end
+
+      it "updates the rank when a down vote is created" do
+        old_rank = post.rank
+        post.votes.create!(value: -1)
+        expect(post.rank).to eq (old_rank - 1)
+      end
     end
   end
 end
