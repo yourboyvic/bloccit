cf8294195e53fc7ef9fc56710b7240065568d798
vic kim <yourboyvic@gmail.com>
2016-02-24 04:33:42 -0800

    checkpoint 47 done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,12 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
 
-validates :body, length: { minimum: 5 }, presence: true
-validates :user, presence: true
+  validates :body, length: { minimum: 5 }, presence: true
+  validates :user, presence: true
 
-after_create :send_favorite_emails
+  default_scope { order('updated_at DESC') }
+
+  after_create :send_favorite_emails
 
    private
 
@@ -14,4 +16,5 @@ after_create :send_favorite_emails
        FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
      end
    end
+   
 end
